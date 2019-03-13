2cdb10aa635d7c8264e10ab3a3526c44fa432fec
Lukas Koestler <lkskstlr@gmail.com>
2019-03-13 12:14:13 +0100

    removed printf from gpu detection

--- src/gpu_detect.cpp
+++ src/gpu_detect.cpp
@@ -1,8 +1,6 @@
 #include "gpu_detect.hpp"
-#include <stdio.h>
 
 int cuda_get_num_gpus()
 {
-    printf("cuda_get_num_gpus cpp version\n");
     return 0;
 }
--- src/gpu_detect.cu
+++ src/gpu_detect.cu
@@ -1,11 +1,8 @@
 #include "gpu_detect.hpp"
 #include <cuda_runtime.h>
-#include <stdio.h>
 
 int cuda_get_num_gpus(){
     int count = 0;
     cudaGetDeviceCount(&count);
-    printf("cuda_get_num_gpus cu version count = %d\n", count);
-
     return count;
 }
